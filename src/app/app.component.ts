048bd37cea9c2ba3d3be6752f0cc971f21f9337a
Gabriel Abrão <114771992+ggabrao@users.noreply.github.com>
2024-09-30 10:11:49 -0300

    Initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  myDate: Date;
+
+  constructor() {
+    this.myDate = new Date();
+  }
+
+
 }

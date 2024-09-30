bff2e4b78562ad09b440721d6e2c0b7be1ea855b
Gabriel Abrão <114771992+ggabrao@users.noreply.github.com>
2024-09-30 20:11:48 -0300

    Add experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: 'home', title: 'Home', component: HomeComponent },
-    {
-        path: 'experience',
-        title: 'Experience',
-        loadComponent: () => import('./experience/experience.component').then(m => m.ExperienceComponent)
-    },
-    {
-        path: 'projects',
-        title: 'Projects',
-        loadComponent: () => import('./projects/projects.component').then(m => m.ProjectsComponent)
-    },
+    { path: 'home', title: 'Home', component: AppComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];

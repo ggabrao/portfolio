61ccd07ba56b1433e3feaa050710f1027aab723d
Gabriel Abrão <114771992+ggabrao@users.noreply.github.com>
2024-10-01 20:05:17 -0300

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { DatePipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, DatePipe, NgClass, FormsModule],
+  imports: [DatePipe, NgClass, FormsModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {

4455e38accc2800ae43153ba3492734089dfdaa1
Gabriel Abrão <114771992+ggabrao@users.noreply.github.com>
2024-10-01 17:04:23 -0300

    Remove routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    { path: 'home', title: 'Home', component: AppComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent }
-];
+export const routes: Routes = [];

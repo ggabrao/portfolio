5e0fadc43769d151b408c5984d63934b81552bc0
gabriel-gga <gguimaraesabrao@gmail.com>
2025-08-18 10:10:24 -0300

    fix fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: [defaultTheme.fontFamily.sans],
       },
     },
   },
